4c3ff501bf3d85643f448b042da8dc07362a18be
davidpa <parrasdavid@gmail.com>
2015-11-16 11:51:37 +0100

    Use variable

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,9 @@
 var React = require("react");
 
 var App = React.createClass({
-    render: function() {
-        return <h1>Hello world!!</h1>;
+  render: function() {
+    var text = "Hello, world";
+    return <h1>{text}!!</h1>;
     }
-});
-
+  });
 React.render(<App/>, document.getElementById("example"));
