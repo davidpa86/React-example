0df2dfa56f7e87a50079fee7a09e367250284228
davidpa <parrasdavid@gmail.com>
2015-11-16 19:49:25 +0100

    Playing with state

--- src/app.jsx
+++ src/app.jsx
@@ -20,5 +20,29 @@ var List = React.createClass({
     }
 });
 
+var Water = React.createClass({
+  getInitialState : function () {
+    return {
+      temperature : 20
+    };
+  },
+  render : function (){
+    var waterState;
+    if (this.state.temperature <= 0)
+    {
+        waterState = "solid";
+    }
+    else if (this.state.temperature >= 100)
+    {
+      waterState = "gas";
+    }
+    else {
+      waterState = "liquid";
+    }
+    return <h2>At temperature {this.state.temperature}, water is {waterState}</h2>;
+  }
+});
+
 //React.render(<App/>, document.getElementById("container"));
-React.render(<List/>, document.getElementById("container"));
+//React.render(<List/>, document.getElementById("container"));
+React.render(<Water/>, document.getElementById("container"));
